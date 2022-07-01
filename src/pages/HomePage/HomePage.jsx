bf71cbe8b2ca2944fa01e6651eff0a74982811bd
sagar-aryal <sagar_yal92@outlook.com>
2022-07-01 19:49:49 +0300

    homepage completed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+import heroImage from "../../assets/heroImage.png";
+
+import { Button, CardMedia, Grid, Typography } from "@mui/material";
+import {
+  buttonStyles,
+  imageItemStyles,
+  mediaCardStyles,
+  typographyStyles,
+} from "./HomePageStyles";
+import { Container } from "@mui/system";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <React.Fragment>
+      <Container>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item>
+            <Grid container>
+              <Grid item>
+                <Typography sx={typographyStyles} variant="h4">
+                  Bringing <br />
+                  Technologies Together
+                </Typography>
+                <Grid item>
+                  <Button sx={buttonStyles} variant="contained">
+                    Learn More
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid sm item sx={imageItemStyles}>
+            <CardMedia
+              component="img"
+              src={heroImage}
+              alt="Hero Image"
+              sx={mediaCardStyles}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default HomePage;
---
+++ src/pages/HomePage/HomePageStyles.js
@@ -0,0 +1,38 @@
+import theme from "../../theme/Theme";
+
+export const imageItemStyles = {
+  maxWidth: "350px",
+  minWidth: "200px",
+  marginTop: "20px",
+  marginLeft: "10vw",
+  [theme.breakpoints.down("sm")]: {
+    minHeight: "200px",
+  },
+};
+
+export const mediaCardStyles = {
+  height: "100%",
+  width: "100%",
+  objectFit: "contain",
+};
+
+export const gridContainerStyles = {
+  color: theme.palette.secondary.main,
+  marginTop: "20px",
+
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "10px",
+  },
+};
+
+export const typographyStyles = {
+  lineHeight: 1.4,
+  color: theme.palette.primary.main,
+  marginTop: "20px",
+};
+
+export const buttonStyles = {
+  color: theme.palette.secondary.main,
+  marginTop: "20px",
+  marginBottom: "20px",
+};
