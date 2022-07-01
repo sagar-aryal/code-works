5d3ba66613ea4842bdb077ce6a4ea7974dca692d
sagar-aryal <sagar_yal92@outlook.com>
2022-07-01 17:28:00 +0300

    footer completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,92 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import { FooterContainer } from "./FooterStyle";
+import { FooterContainer, gridItemStyles, Item } from "./FooterStyle";
+
+import { Grid } from "@mui/material";
+import {
+  FacebookOutlined,
+  Instagram,
+  YouTube,
+  Twitter,
+} from "@mui/icons-material";
 
 const Footer = () => {
-  return <FooterContainer>Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-around"
+        alignItems="center"
+        columnSpacing={{ xs: 1, sm: 2 }}
+      >
+        <Grid item sx={gridItemStyles}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Item item component={Link} to="/">
+              Home
+            </Item>
+            <Item item component={Link} to="/about">
+              About
+            </Item>
+            <Item item component={Link} to="/revolution">
+              Revolution
+            </Item>
+            <Item item component={Link} to="/contact">
+              Contact
+            </Item>
+          </Grid>
+        </Grid>
+        <Grid item sx={gridItemStyles}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Item item component={Link} to="/services">
+              Services
+            </Item>
+            <Item item component={Link} to="/website">
+              Website Development
+            </Item>
+            <Item item component={Link} to="/software">
+              Software Development
+            </Item>
+            <Item item component={Link} to="/mobileapps">
+              Mobile Apps Development
+            </Item>
+          </Grid>
+        </Grid>
+        <Grid item sx={gridItemStyles}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="flex-start"
+            columnGap={2}
+          >
+            <Item item component={Link} to="/">
+              <FacebookOutlined />
+            </Item>
+            <Item item component={Link} to="/">
+              <Instagram />
+            </Item>
+            <Item item component={Link} to="/">
+              <YouTube />
+            </Item>
+            <Item item component={Link} to="/">
+              <Twitter />
+            </Item>
+          </Grid>
+        </Grid>
+      </Grid>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
--- src/components/Footer/FooterStyle.js
+++ src/components/Footer/FooterStyle.js
@@ -1,7 +1,23 @@
+import { Grid } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
+// Customizing footer styles using styled modules from mui packages.
 export const FooterContainer = styled("div")(({ theme }) => ({
   background: theme.palette.primary.main,
   color: theme.palette.primary.contrastText,
-  width: "100%",
+  width: "100vw",
+  zIndex: 1302,
+  position: "relative",
+}));
+
+export const gridItemStyles = {
+  margin: "2rem",
+};
+
+export const Item = styled(Grid)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  textDecoration: "none",
+  "&: hover": {
+    opacity: 0.6,
+  },
 }));
