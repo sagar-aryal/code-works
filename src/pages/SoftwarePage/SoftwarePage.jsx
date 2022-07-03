c4afe2adffce154e14f897c30260ce8617040b82
sagar-aryal <sagar_yal92@outlook.com>
2022-07-03 13:04:13 +0300

    software development page on progress

--- src/pages/SoftwarePage/SoftwarePage.jsx
+++ src/pages/SoftwarePage/SoftwarePage.jsx
@@ -1,7 +1,65 @@
 import React from "react";
 
+import { iconStyles, softwareContainerStyles } from "./SoftwareStyles";
+
+import { Grid, IconButton, Typography, Container } from "@mui/material";
+import { ArrowBack, ArrowForward } from "@mui/icons-material";
+
 const SoftwarePage = () => {
-  return <div>SoftwarePage</div>;
+  return (
+    <React.Fragment>
+      <Container>
+        <Grid container direction="column">
+          <Grid item container>
+            <Grid item>
+              <IconButton style={{ backgroundColor: "transparent" }}>
+                <ArrowBack sx={iconStyles} />
+              </IconButton>
+            </Grid>
+            <Grid
+              item
+              container
+              direction="column"
+              sx={softwareContainerStyles}
+            >
+              <Grid item>
+                <Typography variant="h3">Software Development</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1" paragraph>
+                  Weather we're replacing old software or inventing ne
+                  solutions, Code Works is here to help your business tackle
+                  technology.
+                </Typography>
+                <Typography variant="body1" paragraph>
+                  Using regular commercial software leaves you with a lot of
+                  stuff you don't need, without some of the stuff you do need,
+                  and ultimately controls the way you work. Without using any
+                  software at all your risk falling behind competitore and
+                  missing out on huge savings from increased efficiency.
+                </Typography>
+                <Typography variant="body1" paragraph>
+                  Our custom solution is designed from the ground up with your
+                  needs, wants and goals at the core. This collaborative process
+                  produces finely tuned software that is much more efffective at
+                  improving your workflow and reducing costs than generalized
+                  options.
+                </Typography>
+                <Typography variant="body1" paragraph>
+                  We create exactly what you wantn, exactly how you want it.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <IconButton style={{ backgroundColor: "transparent" }}>
+                <ArrowForward sx={iconStyles} />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default SoftwarePage;
---
+++ src/pages/SoftwarePage/SoftwareStyles.js
@@ -0,0 +1,10 @@
+import theme from "../../theme/Theme";
+
+export const softwareContainerStyles = {
+  maxWidth: "40rem",
+};
+
+export const iconStyles = {
+  color: theme.palette.primary.main,
+  marginTop: "0.5rem",
+};
